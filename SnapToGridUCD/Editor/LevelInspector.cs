305fb4165642525d0ec8b4263168d0f5574e02f2
bchapel <svc-student@student.directory.jccc>
2018-03-22 19:18:02 -0500

    Adding Custom Inspector, and class work

---
+++ SnapToGridUCD/Editor/LevelInspector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelClass))]
+
+public class LevelInspector : Editor {
+
+    private LevelClass _myTarget;
+    public int rows = 20;
+    public int cols = 20;
+
+    private void OnEnable()
+    {
+        _myTarget = (LevelClass)this.target;
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+
+
+        GUILayout.Label("Columns: " + _myTarget.cols);
+        GUILayout.Label("Rows: " + _myTarget.rows);
+
+       
+       rows = EditorGUILayout.IntField("Rows", rows);
+       cols = EditorGUILayout.IntField("Columns", cols);
+
+
+        bool resize = GUILayout.Button("Resize Level");
+        if (resize)
+        {
+            _myTarget.rows = rows;
+            _myTarget.cols = cols;
+        }
+
+        if (rows == _myTarget.rows && cols == _myTarget.cols)
+            GUI.enabled = false;
+        else
+            GUI.enabled = true;
+
+        bool reset = GUILayout.Button("Reset");
+        if (reset)
+        {
+            rows = _myTarget.rows;
+            cols = _myTarget.cols;
+        }
+
+        GUI.enabled = true;
+
+        _myTarget.totalTime = EditorGUILayout.FloatField("Current level time", _myTarget.totalTime);
+        _myTarget.gravity = EditorGUILayout.FloatField("Gravity Magnitude", _myTarget.gravity);
+
+        _myTarget.bgm = (AudioClip)EditorGUILayout.ObjectField("Background Music", _myTarget.bgm, typeof(AudioClip), true);
+        _myTarget.background = (Sprite)EditorGUILayout.ObjectField("Background Display", _myTarget.background, typeof(Sprite), false);
+    }
+}
---
+++ SnapToGridUCD/LevelClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelClass : MonoBehaviour {
+
+    public int rows = 10;
+    public int cols = 20;
+
+    public float totalTime = 60f;
+    public float gravity = -30f;
+
+    public AudioClip bgm;
+    public Sprite background;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
